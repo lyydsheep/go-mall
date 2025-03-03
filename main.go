66e243d2fb91a6e1a3af0148fa994b6918548853
lyydsheep <2230561977@qq.com>
2025-03-03 21:42:53 +0800

    error定制化，串联错误链

--- main.go
+++ main.go
@@ -1,37 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"github.com/faiz/go-mall/common/errcode"
 	log "github.com/faiz/go-mall/common/logger"
 	"github.com/faiz/go-mall/common/middleware"
 	_ "github.com/faiz/go-mall/config"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 	r.Use(middleware.StartTrace(), middleware.LogAccess(), middleware.PanicRecorder())
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	r.GET("/testError", func(c *gin.Context) {
+		err := errors.New("gorm error")
+		aErr := errcode.Wrap("包装", err)
+		bErr := errcode.Wrap("再包装", aErr)
+		log.New(c).Error("记录错误", "err", bErr)
+		domainErr := errors.New("domain Error")
+		apiErr := errcode.ErrServer.WithCause(domainErr)
+		log.New(c).Error("记录错误", "err", apiErr)
+		c.JSON(apiErr.HttpStatusCode(), gin.H{
+			"code":    apiErr.GetCode(),
+			"message": apiErr.GetMsg(),
 		})
-	})
-
-	r.GET("/logTest", func(c *gin.Context) {
-		log.New(c).Info("this is a test", "test", "begin")
-		c.JSON(http.StatusOK, gin.H{
-			"result": "ok",
-		})
-	})
-	r.POST("/testAccessLog", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"result": "ok",
-		})
-	})
-	r.GET("/testPanic", func(c *gin.Context) {
-		fmt.Println("test panic")
-		panic("test panic")
 	})
 	err := r.Run("localhost:8080")
 	if err != nil {
